abb74ab76d0ec17e1fdb42b2969bd4238eb9ba8b
chriskilinc <chriskilinc@gmail.com>
2022-04-02 16:07:59 +0200

    tidying up WIP

--- src/scales.js
+++ src/scales.js
@@ -4,11 +4,28 @@ export const scales = [
         altName: "A Major",
         notes: ["A", "B", "C#", "D", "E", "F#", "G#"],
         intervals: [2, 2, 1, 2, 2, 2, 1],
+        type: "Ionian",
+    },
+    {
+        name: "B Aeolian",
+        altName: "B Minor",
+        notes: ["B", "C#", "D", "E", "F#", "G", "A"],
+        intervals: [2, 1, 2, 2, 1, 2, 2],
+        type: "Aeolian",
+    },
+    {
+        name: "B Harmonic Minor",
+        altName: "B Harmonic Minor",
+        notes: ["B", "C#", "D ", "E", "F#", "G", "A#"],
+        intervals: [2, 1, 2, 2, 1, 3, 1],
+        type: "Harmonic Minor",
     },
     {
         name: "C Ionian",
         altName: "C Major",
         notes: ["C", "D", "E", "F", "G", "A", "B"],
         intervals: [2, 2, 1, 2, 2, 2, 1],
-    }
+        type: "Ionian",
+    },
 ];
+
