bd94820b7d1789e94bf2531ef608476e78d7d9b2
chriskilinc <chriskilinc@gmail.com>
2022-06-03 22:05:41 +0200

    Fix black keys visual text alignment

--- src/App.js
+++ src/App.js
@@ -126,10 +126,13 @@ function App() {
               {piano.map((note, i, arr) => {
                 const reactKey = `${note}-${i}`;
                 const active = scale.notes.includes(note).toString();
-                const pianoKey = note.includes("#") ? "black" : "white";
+                const isBlack = note.includes("#");
+                const pianoKey = isBlack ? "black" : "white";
                 const isKey = (note == scale.notes[0]).toString();
                 const classes = `key ${pianoKey} ${note}`;
-                return <div key={reactKey} note={note} active={active} iskey={isKey} className={classes}><p>{note}</p></div>
+                const notes = note.split("/");  // Only aplicable for black notes, ex: C#/Db
+                const innerKey = isBlack ? <span><p>{notes[0]}</p><p></p><p>{notes[1]}</p></span> : <p>{note}</p>;
+                return <div key={reactKey} note={note} active={active} iskey={isKey} className={classes}>{innerKey}</div>
               })}
             </div>
           </section>
